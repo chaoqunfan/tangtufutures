54498c2524e008193bf8a12791cc7dc427d0d9b8
chaoqun.fan <18334705985@163.com>
2022-06-18 20:03:27 +0800

    20220618

---
+++ 黄香蕉苹果.js
@@ -0,0 +1,1 @@
+function huangxiangjiaopingguo(){myChart.setOption({"title": {"text": "黄香蕉苹果"}, "xAxis": {"data": ["2022-06-12", "2022-06-13", "2022-06-14", "2022-06-15", "2022-06-16", "2022-06-17"]}, "series": [{"name": "兰州国际高原夏菜副食品采购中心", "type": "line", "data": [6.3, 6.21, 6.12, 6.3, 6.12, 6.02]}]});}
